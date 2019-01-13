cfe294a93d103c93d771ba63f87a1cae57ed907b
YoungHoSin <dydgh6809@gmail.com>
2019-01-13 15:48:49 +0900

    MyCat 오류.
    오류의 이유를 모름.

---
+++ MyCat/WindowsFormsApp1/MyCat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuteCat
+{
+    public partial class Form1 : Form
+    {
+        private Cat MyCat = new Cat("Lucas", 1);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+    
+}
---
+++ MyCat/WindowsFormsApp1/cat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CuteCat;
+
+namespace CuteCat
+{
+    public class Cat
+    {
+        private string Name;
+        public int Age;
+
+        private int Happiness = 50;
+
+        public Cat(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void GetBored()
+        {
+            Happiness = Happiness - 10;
+
+            if (Happiness > 100)
+                Happiness = 100;
+        }
+
+        public void play()
+        {
+            Happiness = Happiness + 10;
+
+            if (Happiness > 100)
+                Happiness = 100;        
+        }
+
+        public void Eat()
+        {
+            Happiness = Happiness + 20;
+
+            if (Happiness > 100)
+                Happiness = 100;
+        }
+
+        public string Exprss()
+        {
+            string message = "";
+
+            if (Happiness >= 80)
+                message = "I'm very happy.";
+            else if (Happiness >= 60)
+                message = "I'm happy";
+            else if (Happiness >= 40)
+                message = "I'm so so.";
+            else if (Happiness >= 20)
+                message = "I'm gloomy.";
+            else
+                message = "I'm sad.";
+
+            return this.Name + ": " + message;
+        }
+    }
+}
